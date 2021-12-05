06e5629949ab510c1adf8321b3c0c8eb700921ae
Alexey-og <54952300+Alexey-og@users.noreply.github.com>
2021-12-05 18:13:27 +0300

    Перевод простых компонентов в классы

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import { createTripInfoTemplate } from './view/trip-info.js';
-import { createTripNavigationTemplate } from './view/trip-navigation.js';
-import { createTripFiltersTemplate } from './view/trip-filters.js';
-import { createTripSortTemplate } from './view/trip-sort.js';
-import { createTripListTemplate } from './view/trip-list.js';
+import TripInfoView from './view/trip-info.js';
+import TripNavigationView from './view/trip-navigation.js';
+
+import TripFiltersView from './view/trip-filters.js';
+import TripSortView from './view/trip-sort.js';
+import TripListView from './view/trip-list.js';
 import { createTripItemTemplate } from './view/trip-item.js';
 import { createAddNewPointTemplate } from './view/add-new-point.js';
 
+import { renderTemplate, renderElement, RenderPosition } from './utils/render.js';
 import { getRandomInteger } from './utils/mocks.js';
 import { createTripList } from './mock/trip.js';
-
 
 const TRIP_POINT_MIN_QUANTITY = 15;
 const TRIP_POINT_MAX_QUANTITY = 25;
@@ -16,20 +17,16 @@ const pointsQuantity = getRandomInteger(TRIP_POINT_MIN_QUANTITY, TRIP_POINT_MAX_
 
 const pointsList = createTripList(pointsQuantity);
 
-const renderTemplate = (container, template, place = 'beforeend') => {
-  container.insertAdjacentHTML(place, template);
-};
-
 const tripMainElement = document.querySelector('.trip-main');
 const tripNavigationElement = document.querySelector('.trip-controls__navigation');
 const tripFiltersElement = document.querySelector('.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
-renderTemplate(tripMainElement, createTripInfoTemplate(), 'afterbegin');
-renderTemplate(tripNavigationElement, createTripNavigationTemplate());
-renderTemplate(tripFiltersElement, createTripFiltersTemplate());
-renderTemplate(tripEventsElement, createTripSortTemplate(), 'afterbegin');
-renderTemplate(tripEventsElement, createTripListTemplate());
+renderElement(tripMainElement, new TripInfoView().element, RenderPosition.AFTERBEGIN);
+renderElement(tripNavigationElement, new TripNavigationView().element, RenderPosition.BEFOREEND);
+renderElement(tripFiltersElement, new TripFiltersView().element, RenderPosition.BEFOREEND);
+renderElement(tripEventsElement, new TripSortView().element, RenderPosition.AFTERBEGIN);
+renderElement(tripEventsElement, new TripListView().element, RenderPosition.BEFOREEND);
 
 const tripListElement = document.querySelector('.trip-events__list');
 
--- src/view/trip-list.js
+++ src/view/trip-list.js
@@ -1,3 +1,25 @@
-export const createTripListTemplate = () => (
+import { createElement } from '../utils/render.js';
+
+const createTripListTemplate = () => (
   '<ul class="trip-events__list"></ul>'
 );
+
+export default class TripNavigationView {
+  #element = null;
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  get template() {
+    return createTripListTemplate();
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
